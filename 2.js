5e9c2ca10cd16dd7dc9eee44c1ea24c48b22ded9
Julien Rebetez <julien@fhtagn.net>
2021-08-03 13:40:58 +0200

    Make Token carry file Location information to print better error messages

--- 2.js
+++ 2.js
@@ -1,12 +1,12 @@
 function my_function x y {
   while x > 0 {
-    x = x - 1
-    call print '42'
+    x = x - 1;
+    call print '42';
   }
-  return x
+  return x;
 }
 
 function main {
-  var x = 2 + 3
-  x = call my_function x 2
+  var x = 2 + 3;
+  x = call my_function x 2;
 }
--- src/main.cc
+++ src/main.cc
@@ -31,6 +31,17 @@ class Panic : public std::exception {
   std::string reason_;
 };
 
+struct Location {
+  Location(int line, int col) : line(line), col(col) {}
+
+  int line;
+  int col;
+
+  std::string ToString() const {
+    return "line=" + std::to_string(line) + ", col=" + std::to_string(col);
+  }
+};
+
 class SourceFile {
  public:
   SourceFile(const string& filename) {
@@ -49,17 +60,29 @@ class SourceFile {
     return static_cast<char>(file_.peek());
   }
 
-  char Next() {
+  std::tuple<char, Location> Next() {
     if (IsEnd()) {
       throw EOFException();
     }
-    char c;
-    file_.get(c);
-    return c;
+    const char c = _get();
+    return std::make_tuple(c, Location(line_, col_));
   }
 
  private:
+  char _get() {
+    char c;
+    file_.get(c);
+    if (c == '\n') {
+      line_++;
+      col_ = 0;
+    } else {
+      col_++;
+    }
+    return c;
+  }
   std::ifstream file_;
+  int line_ = 1;
+  int col_ = 1;
 };
 
 struct Token {
@@ -82,9 +105,50 @@ struct Token {
     SepComma,
     SepQuote
   };
-  Token(Type type, const string& val) : type(type), value(val) {}
+  Token(Type type, const string& val, const Location& loc)
+      : type(type), value(val), location(loc) {}
   Type type;
   string value;
+  Location location;
+
+  static const string TypeToString(Token::Type type) {
+    switch (type) {
+      case Type::DecimalLiteral:
+        return "DecimalLiteral";
+      case Type::OtherPunctuator:
+        return "OtherPunctuator";
+      case Type::KwdFunction:
+        return "KwdFunction";
+      case Type::KwdVar:
+        return "KwdVar";
+      case Type::KwdWhile:
+        return "KwdWhile";
+      case Type::Identifier:
+        return "Identifier";
+      case Type::OpPlus:
+        return "OpPlus";
+      case Type::OpMinus:
+        return "OpMinus";
+      case Type::OpEqual:
+        return "OpEqual";
+      case Type::OpGreaterThan:
+        return "OpGreaterThan";
+      case Type::SepLeftPar:
+        return "SepLeftPar";
+      case Type::SepRightPar:
+        return "SepRightPar";
+      case Type::SepLeftBrace:
+        return "SepLeftBrace";
+      case Type::SepRightBrace:
+        return "SepRightBrace";
+      case Type::SepSemicolon:
+        return "SepSemicolon";
+      case Type::SepComma:
+        return "SepComma";
+      case Type::SepQuote:
+        return "SepQuote";
+    }
+  }
 
   const string ToString() const {
     switch (type) {
@@ -138,33 +202,42 @@ const std::unordered_map<int, Token::Type> kSimpleTokens = {
 
 Token ReadDigit(SourceFile* file) {
   string digit = "";
+  std::optional<Location> start_loc;
   while (true) {
     char c = file->Peek();
     if (file->IsEnd() || !std::isdigit(static_cast<int>(c))) {
       break;
     }
-    c = file->Next();
-    digit.push_back(c);
+    auto [rc, location] = file->Next();
+    if (!start_loc) {
+      start_loc = location;
+    }
+
+    digit.push_back(rc);
   }
-  return Token(Token::Type::DecimalLiteral, digit);
+  return Token(Token::Type::DecimalLiteral, digit, *start_loc);
 }
 
 // Reads either an Identifier or a Keyword
 Token ReadLiteral(SourceFile* file) {
   string lit = "";
+  std::optional<Location> start_loc;
   while (true) {
     char c = file->Peek();
     if (file->IsEnd() || !(std::isalpha(static_cast<int>(c)) |
                            std::isdigit(static_cast<int>(c)) | c == '_')) {
       break;
     }
-    c = file->Next();
-    lit.push_back(c);
+    auto [rc, location] = file->Next();
+    if (!start_loc) {
+      start_loc = location;
+    }
+    lit.push_back(rc);
   }
   if (kKeywords.find(lit) != kKeywords.end()) {
-    return Token(kKeywords.at(lit), lit);
+    return Token(kKeywords.at(lit), lit, *start_loc);
   } else {
-    return Token(Token::Type::Identifier, lit);
+    return Token(Token::Type::Identifier, lit, *start_loc);
   }
 }
 
@@ -180,8 +253,9 @@ vector<Token> lex(SourceFile* file) {
     } else if (std::isalpha(static_cast<int>(c))) {
       tokens.push_back(ReadLiteral(file));
     } else if (kSimpleTokens.find(static_cast<int>(c)) != kSimpleTokens.end()) {
+      auto [rc, location] = file->Next();
       tokens.push_back(Token(kSimpleTokens.at(static_cast<int>(c)),
-                             std::string(1, file->Next())));
+                             std::string(1, rc), location));
     } else if (c == ' ' || c == '\n') {
       file->Next();
     } else {
@@ -193,48 +267,84 @@ vector<Token> lex(SourceFile* file) {
   return tokens;
 }
 
+struct ASTNode {
+  Token token;
+  vector<ASTNode> children;
+
+  void Visit(const std::function<void(const ASTNode& node)>& visitor) const {
+    visitor(*this);
+    for (const ASTNode& node : children) {
+      node.Visit(visitor);
+    }
+  }
+};
+
 class AST {
  public:
   AST(const vector<Token>& tokens);
 
-  struct Node {
-    Token token;
-    vector<Node> children;
-  };
+  void Visit(const std::function<void(const ASTNode& node)>& visitor) const {
+    for (const ASTNode& node : functions) {
+      node.Visit(visitor);
+    }
+  }
 
  private:
-  vector<Node> functions;
+  vector<ASTNode> functions;
 };
 
 Token AssertNext(queue<Token>& tokens, Token::Type type) {
   Token t = tokens.front();
   if (t.type != type) {
-    throw Panic("Incorrect type");
+    throw Panic("Incorrect type, expected " + Token::TypeToString(type) +
+                ", got " + Token::TypeToString(t.type) + ", at " +
+                t.location.ToString());
   }
   tokens.pop();
   return t;
 }
 
-AST::Node ConsumeStatement(queue<Token>& tokens) {}
+ASTNode ConsumeStatement(queue<Token>& tokens) {
+  vector<Token> statement;
+  while (tokens.size() > 0 &&
+         tokens.front().type != Token::Type::SepSemicolon) {
+    statement.push_back(tokens.front());
+    tokens.pop();
+  }
+  AssertNext(tokens, Token::Type::SepSemicolon);
+  return ASTNode{// TODO: Placeholder
+                 .token =
+                     Token(Token::Type::SepSemicolon, ";", Location(42, 42))};
+}
 
-AST::Node ConsumeFunction(queue<Token>& tokens) {
-  AssertNext(tokens, Token::Type::KwdFunction);
+ASTNode ConsumeFunction(queue<Token>& tokens) {
+  auto function = AssertNext(tokens, Token::Type::KwdFunction);
   auto name = AssertNext(tokens, Token::Type::Identifier);
   // Arguments
   vector<Token> arguments;
-  while (tokens.front().type != Token::Type::SepLeftBrace) {
+  while (tokens.size() > 0 &&
+         tokens.front().type != Token::Type::SepLeftBrace) {
     arguments.push_back(AssertNext(tokens, Token::Type::Identifier));
   }
 
   AssertNext(tokens, Token::Type::SepLeftBrace);
-  // Body
+  vector<ASTNode> statements;
+  while (tokens.size() > 0 &&
+         tokens.front().type != Token::Type::SepRightBrace) {
+    statements.push_back(ConsumeStatement(tokens));
+  }
   AssertNext(tokens, Token::Type::SepRightBrace);
+  return ASTNode{.token = function};
 }
 
 AST::AST(const vector<Token>& tokens) {
   queue<Token> to_consume;
   for (const Token& t : tokens) {
     to_consume.push(t);
+  }
+
+  while (tokens.size() > 0) {
+    functions.push_back(ConsumeFunction(to_consume));
   }
 }
 
@@ -251,5 +361,10 @@ int main(int argc, char** argv) {
     std::cout << s.ToString() << " ";
   }
   std::cout << std::endl;
+
+  AST ast(tokens);
+  ast.Visit([](const ASTNode& node) {
+    std::cout << Token::TypeToString(node.token.type) << std::endl;
+  });
   return EXIT_SUCCESS;
 }
